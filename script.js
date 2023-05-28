e4adc5ea7869df1456c894ede01877fe6c9bbbec
surajyadav471 <115660802+surajyadav471@users.noreply.github.com>
2023-05-28 10:55:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,11 @@
 //your JS code here. If required.
-const buttonsContainer = document.getElementById("buttons");
+function play() { 
+     var audio = document.getElementById("audio"); 
+     audio.play(); 
+   } 
+ function play2(){ 
+         var audio = document.getElementById("audio2"); 
+     audio.play2(); 
+ }
 
 
-const sounds = ["sound1.mp3", "sound2.mp3", "sound3.mp3", "sound4.mp3"];
-
-
-sounds.forEach(sound => {
-  const audioElement = new Audio(`sounds/${sound}`);
-  buttonsContainer.appendChild(audioElement);
-});
-
-
-function playSound(soundFile) {
-
-  stopSound();
-
-  
-  const audioElement = buttonsContainer.querySelector(`audio[src="sounds/${soundFile}"]`);
-
- 
-  audioElement.play();
-}
-
-
-function stopSound() {
-  
-  const audioElements = buttonsContainer.querySelectorAll("audio");
-
- 
-  audioElements.forEach(audioElement => {
-    audioElement.pause();
-    audioElement.currentTime = 0;
-  });
-}
